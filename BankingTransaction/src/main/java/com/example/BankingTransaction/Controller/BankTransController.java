ca715e715f3045b81e72755f44c89d7a38114fd0
Rutujshahare <shaharerutuj23@gmail.com>
2026-02-14 12:27:06 +0530

    controller and pom code added

---
+++ BankingTransaction/src/main/java/com/example/BankingTransaction/Controller/BankTransController.java
@@ -0,0 +1,26 @@
+package com.example.BankingTransaction.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.BankingTransaction.DTO.Bank;
+import com.example.BankingTransaction.Service.BankingTransService;
+
+@RestController
+@RequestMapping("/Bank")
+public class BankTransController {
+
+	@Autowired
+	private BankingTransService bankingTransService;
+	
+	@PostMapping("/addTransaction")
+	public ResponseEntity<String> addTransactionMessage(@RequestBody Bank bank){
+		String responseString=this.bankingTransService.updateTransaction(bank);
+		return new ResponseEntity<String>(responseString,HttpStatus.OK);
+	}
+}
